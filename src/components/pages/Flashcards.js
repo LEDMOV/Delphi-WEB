e1e936b886627d34e6b55a5f51c58f4553f39666
lxdv <ledmov@outlook.com>
2025-01-17 00:46:16 +0000

    fixed file location typo... again & again...

--- src/components/pages/Flashcards.js
+++ src/components/pages/Flashcards.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { db, auth } from '.vercel/path0/components/pages/firebase.js'; // Correct relative path
+import { db, auth } from '.delphi-web/.vercel/path0/components/pages/firebase.js'; // Correct relative path
 import { collection, addDoc, getDocs, query, where } from 'firebase/firestore';
 import './Flashcards.css';
 
