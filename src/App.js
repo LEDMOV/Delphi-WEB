c2aa36b8d47b86c20b625b94176087ba37e2b1f3
lxdv <ledmov@outlook.com>
2025-01-17 00:57:55 +0000

    fixed login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './components/layouts/Header';
 import Auth from './components/Auth';
 import Flashcards from './components/pages/Flashcards';
 
@@ -12,6 +12,10 @@ function App() {
       <Switch>
         <Route path="/login" component={Auth} />
         <Route path="/flashcards" component={Flashcards} />
+        <Route path="/podcasts" component={Podcasts} /> {/* Add Podcasts component if needed */}
+        <Route path="/chatbot" component={Chatbot} /> {/* Add Chatbot component if needed */}
+        <Route path="/notes" component={Notes} /> {/* Add Notes component if needed */}
+        <Route exact path="/" component={Home} /> {/* Add Home component if needed */}
       </Switch>
     </Router>
   );
